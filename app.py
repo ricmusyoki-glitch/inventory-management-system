4ccd87b00393f85de6f878eff81eff3c3d80be95
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-07-12 11:46:30 +0300

    Implement inventory retrieval endpoints

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ def home():
 def get_inventory():
     return jsonify(inventory)
 
+@app.route("/inventory/<int:item_id>", methods=["GET"])
+def get_inventory_item(item_id):
+    for item in inventory:
+        if item["id"] == item_id:
+            return jsonify(item)
+
+    return jsonify({
+        "error": "Item not found"
+    }), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
